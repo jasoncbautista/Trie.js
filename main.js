6c0f4d2d6e6f9f41164fe5b2de9e1c7acc12a1bb
Jason B <bjason@gmail.com>
2013-11-22 16:55:41 -0800

    Skeleton of trie

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
 console.log("HI");
+
+// /Here is an example of a node:
+var node = {
+        children: []
+    ,   value: ""
+    ,   results: []
+}
+
+var Trie =  function(){
+    // We initialize our node with an empty
+    this.head = {
+            children: []
+        ,   value: ""
+        ,   results: []
+    };
+};
+
+Trie.prototype.insert = function(value) {
+    console.log("value", value);
+    if (prefix.length === 0 || prefix == " "){
+        return;
+    }
+
+
+};
+
+Trie.prototype._insertHelper= function(){
+}
+
+Trie.prototype._lookupHeler = function(){
+};
+Trie.prototype.lookup = function(prefix) {
+    console.log('prefix', prefix);
+    if (prefix.length === 0 || prefix == " "){
+        return [];
+    }
+
+    return [];
+};
+
+
+
